456c985fc781e72ca7b68fdb59452b017d20e587
Vadim Lepishko <lepishko2000@gmail.com>
2021-09-06 22:02:31 +0300

    fix name variable FLASK_DEBUG in file config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 
 class Config:
     """Base class for  project configuration"""
-    DEBUG = os.getenv('FLASK_DEUBG')
+    DEBUG = os.getenv('FLASK_DEBUG')
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
